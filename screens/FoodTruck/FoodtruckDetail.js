4e69707ce71c78775376bb54cac891dcae35676b
Danial Khan <danialkhan@bitfoot.org>
2023-01-26 05:37:17 +0500

    destinations work.

--- screens/FoodTruck/FoodtruckDetail.js
+++ screens/FoodTruck/FoodtruckDetail.js
@@ -9,28 +9,51 @@ import firebase from "../../firebase.js";
 import * as Location from "expo-location";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import MapView, { Marker } from "react-native-maps";
+import MapViewDirections from 'react-native-maps-directions';
 
 export default function FoodtruckDetail({ route, navigation }) {
   const [foods, setFood] = useState([]);
   const [usertype, setUserType] = useState("");
   const [email, setEmail] = useState("");
   const [showMap, setShowMap] = useState(false);
-  const [lat, setLat] = useState(foods[0]?.latitude || 37.78825);
-  const [long, setLong] = useState(foods[0]?.longitude || 37.78825);
+  const [lat, setLat] = useState(foods[0]?.latitude);
+  const [long, setLong] = useState(foods[0]?.longitude);
+  const [distance, setDistance] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const origin = {latitude: 25.0159198, longitude: 67.1294916};
+    const destination = {latitude: 24.895118240675693, longitude: 67.11689262666305};
+    const GOOGLE_MAPS_APIKEY = 'AIzaSyB5KZy-WiNvS_l7AjO-lV-eNdSaPBVLuyg';
+    var mapView = null;
 
   const getData = () => {
     try {
       AsyncStorage.getItem("userType").then((valuez) => {
-        if (valuez != null) {
+        if (valuez) {
           setUserType(JSON.parse(valuez));
+
+          firebase
+            .firestore()
+            .collection("resturant")
+            .doc(route.params.id)
+            .onSnapshot((snapshot) => {
+              let _newData = snapshot.data();
+
+              setLat(_newData.latitude);
+              setLong(_newData.longitude);
+              if (JSON.parse(valuez) === "business") {
+                shouldUpdateLocation();
+              }
+              setShowMap(true);
+            });
         }
       });
     } catch (error) {
       console.log(error);
     }
+
     try {
       AsyncStorage.getItem("email").then((value) => {
-        if (value != null) {
+        if (value) {
           setEmail(JSON.parse(value));
         }
       });
@@ -48,9 +71,12 @@ export default function FoodtruckDetail({ route, navigation }) {
           {},
           async (location) => {
             const { latitude, longitude } = location.coords;
+
             let thisMarkerID = null;
 
-            db.collection("mapmarkers").where('truckId', '==', route.params.id).onSnapshot((snapshot) => {
+            db.collection("mapmarkers")
+              .where("truckId", "==", route.params.id)
+              .onSnapshot((snapshot) => {
                 thisMarkerID = snapshot?.docs[0]?.id;
               });
 
@@ -60,18 +86,18 @@ export default function FoodtruckDetail({ route, navigation }) {
             });
 
             if (thisMarkerID) {
-                await db.collection("mapmarkers").doc(thisMarkerID).update({
-                    latitude,
-                    longitude,
-                    isonline: true
-                });
+              await db.collection("mapmarkers").doc(thisMarkerID).update({
+                latitude,
+                longitude,
+                isonline: true,
+              });
             } else {
-                db.collection("mapmarkers").add({
-                    latitude,
-                    longitude,
-                    isonline: true,
-                    truckId: route.params.id
-                })
+              db.collection("mapmarkers").add({
+                latitude,
+                longitude,
+                isonline: true,
+                truckId: route.params.id,
+              });
             }
           }
         );
@@ -90,18 +116,6 @@ export default function FoodtruckDetail({ route, navigation }) {
       .onSnapshot((snapshot) => {
         let _newData = snapshot?.docs[0]?.data();
         setFood([_newData]);
-      });
-
-    const db = firebase.firestore();
-    db.collection("resturant")
-      .doc(route.params.id)
-      .onSnapshot((snapshot) => {
-        let _newData = snapshot.data();
-        
-        setLat(_newData.latitude);
-        setLong(_newData.longitude);
-        shouldUpdateLocation();
-        setShowMap(true);
       });
   }, []);
 
@@ -126,10 +140,36 @@ export default function FoodtruckDetail({ route, navigation }) {
                 initialRegion={{
                   latitude: lat,
                   longitude: long,
-                  latitudeDelta: 0.0,
-                  longitudeDelta: 0.0,
+                  latitudeDelta: 0.15,
+                  longitudeDelta: 0.25,
                 }}
+                showsUserLocation={true}
+                ref={c => mapView = c}
               >
+                <MapViewDirections
+                    origin={origin}
+                    destination={destination}
+                    apikey={GOOGLE_MAPS_APIKEY}
+                    strokeWidth={4}
+                    strokeColor="lightblue"
+                    precision="low"
+                    timePrecision="now"
+                    mode="DRIVING"
+                    tappable={true}
+                    onReady={result => {
+                        setDistance(result.distance);
+                        setDuration(result.duration);
+                        
+                        mapView.fitToCoordinates(result.coordinates, {
+                          edgePadding: {
+                            right: (100 / 20),
+                            bottom: (100 / 20),
+                            left: (100 / 20),
+                            top: (100 / 20),
+                          }
+                        });
+                      }}
+                />
                 <Marker
                   key={`${new Date().getMilliseconds()}`}
                   coordinate={{
@@ -139,6 +179,9 @@ export default function FoodtruckDetail({ route, navigation }) {
                   title={route.params.name}
                   description={route.params.about}
                 />
+                <View>
+                    <Text>distance: {distance.toFixed(2)} km - time: {duration.toFixed(0)} mins</Text>
+                </View>
               </MapView>
             ) : (
               <></>
--- screens/Map/Map.js
+++ screens/Map/Map.js
@@ -1,18 +1,19 @@
-
-import {
-  View,
-  StyleSheet,
-} from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import React, { useEffect, useState, useRef } from "react";
-import firebase from '../../firebase.js';
+import firebase from "../../firebase.js";
 import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
-
+import MapViewDirections from "react-native-maps-directions";
 
 export default function Map({ navigation, ...props }) {
   const [markers, setMarkers] = useState([]);
   const [location, setLocation] = useState();
-  
+  const [origin, setOrigin] = useState({}); //{latitude: 25.0159198, longitude: 67.1294916};
+  const [destination, setDestination] = useState({}); //{latitude: 24.895118240675693, longitude: 67.11689262666305};
+  const [distance, setDistance] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const GOOGLE_MAPS_APIKEY = "AIzaSyB5KZy-WiNvS_l7AjO-lV-eNdSaPBVLuyg";
+  var mapView = null;
 
   const getLocation = async () => {
     try {
@@ -20,26 +21,35 @@ export default function Map({ navigation, ...props }) {
         {},
         async (location) => {
           setLocation(location);
+          setOrigin({
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+          });
+          console.log(location);
         }
       );
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   const getMarkers = () => {
     setInterval(async () => {
-      firebase.firestore().collection("mapmarkers").where('isonline', '==', true).onSnapshot(snapshot => {
-        let tempdata = [];
-        
-        snapshot.docs.map(doc => {
-            tempdata.push({...doc.data()})
-        })
-        
-        setMarkers(tempdata);
-        })
-    }, 10000);
-    }
+      firebase
+        .firestore()
+        .collection("mapmarkers")
+        .where("isonline", "==", true)
+        .onSnapshot((snapshot) => {
+          let tempdata = [];
+
+          snapshot.docs.map((doc) => {
+            tempdata.push({ ...doc.data() });
+          });
+
+          setMarkers(tempdata);
+        });
+    }, 5000);
+  };
 
   useEffect(() => {
     getLocation();
@@ -49,7 +59,7 @@ export default function Map({ navigation, ...props }) {
   return (
     <>
       <View style={{ width: "100%", height: "100%" }}>
-      <MapView
+        <MapView
           style={{ width: "100%", height: "100%" }}
           initialRegion={{
             latitude: location?.coords.latitude || 25.0158546,
@@ -59,19 +69,56 @@ export default function Map({ navigation, ...props }) {
           }}
           annotations={markers}
           showsUserLocation={true}
+          ref={(c) => (mapView = c)}
         >
+          <MapViewDirections
+            origin={origin}
+            destination={destination}
+            apikey={GOOGLE_MAPS_APIKEY}
+            strokeWidth={4}
+            strokeColor="lightblue"
+            precision="low"
+            timePrecision="now"
+            mode="DRIVING"
+            tappable={true}
+            onReady={(result) => {
+              setDistance(result.distance);
+              setDuration(result.duration);
+
+              mapView.fitToCoordinates(result.coordinates, {
+                edgePadding: {
+                  right: 100 / 20,
+                  bottom: 100 / 20,
+                  left: 100 / 20,
+                  top: 100 / 20,
+                },
+              });
+            }}
+          />
           {markers.map((mk, i) => (
-              <Marker
-                key={i + `${new Date().getMilliseconds()}`}
-                coordinate={{
+            <Marker
+              key={i + `${new Date().getMilliseconds()}`}
+              coordinate={{
+                latitude: mk.latitude,
+                longitude: mk.longitude,
+              }}
+              tracksViewChanges={true}
+              title={mk.name}
+              description={mk.description}
+              onPress={(res) => {
+                setDestination({
                   latitude: mk.latitude,
-                  longitude: mk.longitude
-                }}
-                tracksViewChanges={true}
-                title={mk.name}
-                description={mk.description}
-              />
-            ))}
+                  longitude: mk.longitude,
+                });
+              }}
+            />
+          ))}
+          <View>
+            <Text>
+              distance: {distance.toFixed(2)} km - time: {duration.toFixed(0)}{" "}
+              mins
+            </Text>
+          </View>
         </MapView>
       </View>
     </>
